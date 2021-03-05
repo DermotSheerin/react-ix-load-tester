0f42d2a9138f6624e550ed566d482e4793675614
Dermot Sheerin <sheerin@avaya.com>
2021-03-05 00:19:36 +0000

    add nginx to react dockerfile 7

---
+++ build/precache-manifest.c84d4e63267a173d6b448186a0bef7ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d579d6635486da790b785edb22a679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f8383d07c336a4064f",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "6e5e1b1c6e4a2b1c677c",
+    "url": "/static/css/main.f80abfda.chunk.css"
+  },
+  {
+    "revision": "57f8383d07c336a4064f",
+    "url": "/static/js/2.686415a0.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.686415a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5e1b1c6e4a2b1c677c",
+    "url": "/static/js/main.df9f8e9e.chunk.js"
+  },
+  {
+    "revision": "825dfa40017beaf65646",
+    "url": "/static/js/runtime-main.30a75f07.js"
+  }
+]);
