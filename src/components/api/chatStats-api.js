c1c1ab974addf1a133a5562efdc23d803c2e811d
Dermot Sheerin <sheerin@avaya.com>
2021-03-18 10:11:01 +0000

    change base port to 8001

--- src/components/api/chatStats-api.js
+++ src/components/api/chatStats-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const baseIP = "127.0.0.1"
-export const port = 8000;
+export const baseIP = "10.134.45.26"
+export const port = 8001;
 
 // export const baseIP = "192.168.178.55"
 // export const port = 8000;
